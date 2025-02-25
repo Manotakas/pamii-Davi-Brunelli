caed842acedc208161d9e43daadc0f8073e9ea99
Manotakas <takedi006@gmail.com>
2025-02-25 17:53:41 -0300

    tutorial sigma

--- aula01/app/index.tsx
+++ aula01/app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet } from 'react-native';
+ import { Image } from 'expo-image'; 
+
+
+const PlaceholderImage = require('@/assets/images/vegeta-careca.jpg');
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+  },
+});
+
